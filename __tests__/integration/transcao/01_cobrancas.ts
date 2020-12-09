801f8f38d9cec9213c309b7633ddbe0cb04aea04
nevr001 <playcar46@gmail.com>
2020-12-09 20:19:40 -0300

    Correção de testes da cobrança
    Corrigido alguns valores esperados pelos testes de cobranças

--- __tests__/integration/transcao/01_cobrancas.ts
+++ __tests__/integration/transcao/01_cobrancas.ts
@@ -1,56 +1,60 @@
 import Juno from './../../../src/index'
-export function cobrancas (juno: Juno): void {
-  describe('Cobranças - Transação', () => {
-    test('Gerar Cobranças', async (done) => {
-      try {
-        const result = await juno.transacao.cobrancas.gerarCobranca({
-          charge: {
-            amount: '15.00',
-            description: 'Compra de Produto X'
-          },
-          billing: {
-            name: 'Cliente Comprador X',
-            document: '06983532422'
-          }
+
+export function cobrancas(juno: Juno): void {
+    describe('Cobranças - Transação', () => {
+        test('Gerar Cobranças', async (done) => {
+            try {
+                const result = await juno.transacao.cobrancas.gerarCobranca({
+                    charge: {
+                        amount: '15.00',
+                        description: 'Compra de Produto X'
+                    },
+                    billing: {
+                        name: 'Cliente Comprador X',
+                        document: '06983532422'
+                    }
+                })
+
+                expect(result).toBeTruthy()
+                done()
+                
+            } catch (error) {
+                done(error)
+            }
         })
-        expect(result).toBeTruthy()
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })
 
-    test('Listar Cobranças', async (done) => {
-      try {
-        const result = await juno.transacao.cobrancas.listarCobrancas({})
-        expect(result._embedded.charges[0]).toBeTruthy()
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })
+        test('Listar Cobranças', async (done) => {
+            try {
+                const result = await juno.transacao.cobrancas.listarCobrancas({})
+                expect(result._embedded.charges[0]).toBeTruthy()
+                done()
+            } catch (error) {
+                done(error)
+            }
+        })
 
-    test('Consultar cobrança', async (done) => {
-      try {
-        const lista = await juno.transacao.cobrancas.listarCobrancas({})
-        const result = await juno.transacao.cobrancas.consultarCobranca(lista._embedded.charges[0].id)
+        test('Consultar cobrança', async (done) => {
+            try {
+                const lista = await juno.transacao.cobrancas.listarCobrancas({})
+                const result = await juno.transacao.cobrancas.consultarCobranca(lista._embedded.charges[0].id)
 
-        expect(result.amount).toBeTruthy()
-        done()
-      } catch (error) {
-        done(error)
-      }
-    })
+                expect(result.amount).toBeTruthy()
+                done()
+            } catch (error) {
+                done(error)
+            }
+        })
 
-    test('Cancelar cobrança', async (done) => {
-      try {
-        const lista = await juno.transacao.cobrancas.listarCobrancas({})
-        const result = await juno.transacao.cobrancas.cancelarCobranca(lista._embedded.charges[1].id)
-        console.log(result)
-        done()
-      } catch (error) {
-        done(error)
-      }
+        test('Cancelar cobrança', async (done) => {
+            try {
+                const lista = await juno.transacao.cobrancas.listarCobrancas({})
+                const result = await juno.transacao.cobrancas.cancelarCobranca(lista._embedded.charges[0].id)
+
+                expect(result).toBe(204)
+                done()
+            } catch (error) {
+                done(error)
+            }
+        })
     })
-  })
 }
