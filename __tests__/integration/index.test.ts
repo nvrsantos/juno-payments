f260c595c0c068289c5866babd6afe437bf04fbb
nevr001 <playcar46@gmail.com>
2020-12-09 20:20:54 -0300

    Configurando a API para os novos testes

--- __tests__/integration/index.test.ts
+++ __tests__/integration/index.test.ts
@@ -4,13 +4,15 @@ import { contasDigitais } from './gestao/01_contasDigitais'
 import { dadosAdicionais } from './gestao/02_dadosAdicionais'
 import { saldo } from './gestao/05_saldo'
 import { cobrancas } from './transcao/01_cobrancas'
+import { pagamentos } from './transcao/02_pagamentos'
 import { assinaturas } from './transcao/03_assinaturas'
 
 const juno = new Juno({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   mode: 'dev',
-  token: process.env.TOKEN
+  token: process.env.TOKEN,
+  publicToken: process.env.PUBLIC_TOKEN
 })
 
 export { juno }
@@ -21,5 +23,6 @@ describe('Init', () => {
   saldo(juno)
 
   cobrancas(juno)
+  pagamentos(juno)
   assinaturas(juno)
 })
